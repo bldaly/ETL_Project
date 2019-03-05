122f2c04963d4401c40a8dc10f953036b71096c2
deedee <dinshawdiana@gmail.com>
2019-03-04 21:25:58 -0700

    with index

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 from flask import Flask, render_template, redirect, Response, request
+from flask_pymongo import PyMongo
+
+app= Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+mongo=PyMongo(app, uri='mongodb://localhost:27017/restaurants_db')
+
+@app.route('/')
+def index():
+    
+    return render_template('index.html')
+   
+
+@app.route('/extract')
+def extract():
+    
+    return render_template('index.html')
+
+
+@app.route('/transform')
+def transform():
+    return render_template('index.html')
+
+@app.route('/load')
+def load():
+    mars_data = scrape_mars.scrape()
+    mars= mongo.db.mars
+    mars.update({}, mars_data, upsert=True)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
