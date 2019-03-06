ace4186497e9b0f6e1fd82ffd7b02ba2a62bdabf
Aritra Kumar Lahiri <lahiri.aritra@gmail.com>
2019-03-05 23:47:40 -0700

    Add route in Flask app for load

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, redirect, Response, request
-from flask_pymongo import PyMongo
+from flask_pymongo import pymongo
 
 app= Flask(__name__)
 
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+# Connect to a database. Will create one if not already available.
+db = client.restaurants_db
+
+
 # Use flask_pymongo to set up mongo connection
-mongo=PyMongo(app, uri='mongodb://localhost:27017/restaurants_db')
+# mongo=PyMongo(app, uri='mongodb://localhost:27017/restaurants_db')
 
 @app.route('/')
 def index():
@@ -24,9 +34,12 @@ def transform():
 
 @app.route('/load')
 def load():
-    mars_data = scrape_mars.scrape()
-    mars= mongo.db.mars
-    mars.update({}, mars_data, upsert=True)
+    restaurants = db.restaurants.find()
+    return render_template("load.html",
+        restaurants=restaurants)
+
+
+
 
 
 if __name__=='__main__':
